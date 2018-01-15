c4a4c27aff9191a289e83b8c0e850499c2bf3dc4
Unknown <at.frizzy@gmail.com>
2018-01-15 16:22:07 +0100

    update erreurExport

--- controller/ControllerExtraction.php
+++ controller/ControllerExtraction.php
@@ -84,7 +84,7 @@ class ControllerExtraction
             if (isset($_GET['p'])) {
                 $p = intval($_GET['p']);
                 if ($p > ModelErreurExport::getNbP()) $p = ModelErreurExport::getNbP();
-                if ($p <= 0) $p=1;
+                if ($p <= 0) $p = 1;
             } else $p = 1;
             $max = ModelErreurExport::getNbP();
             $tab = ModelErreurExport::selectByPage($p);
@@ -319,7 +319,7 @@ class ControllerExtraction
      */
     public static function solvedDepInv()
     {
-        if(isset($_SESSION['login'])) {
+        if (isset($_SESSION['login'])) {
             foreach ($_POST as $cle => $item) {
                 /**
                  * @var $cle string : code d'activité
@@ -329,12 +329,12 @@ class ControllerExtraction
                  */
                 $cle = str_replace("_", " ", $cle);
                 if ($item != 'rien') {
-                    $idErreurs=ModelErreurExport::selectIdErreursDepInv($cle);
-                    if(!$idErreurs);
+                    $idErreurs = ModelErreurExport::selectIdErreursDepInv($cle);
+                    if (!$idErreurs) ;
                     else {
                         // Changer le code d'activité
-                        if(isset($cle[2]))
-                        $cle[2]=$item; // Changement du code Département
+                        if (isset($cle[2]))
+                            $cle[2] = $item; // Changement du code Département
                         foreach ($idErreurs as $idErreur) {
                             ModelErreurExport::update(array(
                                 'idErreur' => $idErreur['idErreur'],
@@ -349,6 +349,57 @@ class ControllerExtraction
                 }
             }
             ControllerExtraction::solveDepInv();
+        } else ControllerUser::connect();
+    }
+
+    /**
+     * Affiche le formulaire de modification d'une erreur
+     *
+     * Si cette erreur n'est pas résolue
+     */
+    public static function update()
+    {
+        if (isset($_SESSION['login'])) {
+            if (isset($_GET['idErreur'])) {
+                $erreur = ModelErreurExport::select($_GET['idErreur']);
+                if (!$erreur) ControllerMain::erreur("Cette erreur n'existe pas");
+                else {
+                    $view = 'update';
+                    $pagetitle = 'Modification d\'une erreur';
+                    require_once File::build_path(array('view', 'view.php'));
+                }
+            } else ControllerMain::erreur("Il manque des informations");
+        } else ControllerUser::connect();
+    }
+
+    public static function updated()
+    {
+        if (isset($_SESSION['login'])) {
+            if (isset($_POST['idErreur']) &&
+                isset($_POST['nomEns']) &&
+                isset($_POST['codeEns']) &&
+                isset($_POST['departementEns']) &&
+                isset($_POST['statut']) &&
+                isset($_POST['typeStatut']) &&
+                isset($_POST['dateCours']) &&
+                isset($_POST['duree']) &&
+                isset($_POST['activitee']) &&
+                isset($_POST['typeActivitee'])) {
+                $data = array(
+                    'idErreur' => $_POST['idErreur'],
+                    'nomEns' => $_POST['nomEns'],
+                    'codeEns' => $_POST['codeEns'],
+                    'departementEns' => $_POST['departementEns'],
+                    'statut' => $_POST['statut'],
+                    'typeStatut' => $_POST['typeStatut'],
+                    'dateCours' => $_POST['dateCours'],
+                    'duree' => $_POST['duree'],
+                    'activitee' => $_POST['activitee'],
+                    'typeActivitee' => $_POST['typeActivitee']
+                );
+                if(ModelErreurExport::update($data)) ControllerExtraction::readAll();
+                else ControllerMain::erreur("Impossible de modifier l'erreur");
+            } else ControllerMain::erreur("Il manque des informations");
         } else ControllerUser::connect();
     }
 }
